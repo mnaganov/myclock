64db79b3e086cd7e1dd82ef9f4c4b564772512e5
Mikhail Naganov <mnaganov@chromium.org>
2016-03-25 22:52:44 -0700

    Displaying time as I want

--- myclock.ino
+++ myclock.ino
@@ -1,17 +1,28 @@
 /* -*- mode: c; -*- */
 #include <ADK.h>
 
+#define LED_LEFT_SEPARATOR 9
+#define LED_RIGHT_SEPARATOR 2
+#define ICON_LOCK 7
+
 ADK L;
 
 void adkPutchar(char c) {
   Serial.write(c);
 }
 
-static void clearScreen(void) {
-  uint32_t i;
-  for(i = 0; i < NUM_LEDS; i++) {
-    L.ledWrite(i, 0, 0, 0);
-  }
+static void displayCurrentTime(uint8_t r, uint8_t g, uint8_t b) {
+  uint16_t year;
+  uint8_t month, day, h, m, s;
+  L.rtcGet(&year, &month, &day, &h, &m, &s);
+  L.ledDrawLetter(0, h / 10 + '0', r, g, b);
+  L.ledDrawLetter(1, h % 10 + '0', r, g, b);
+  L.ledWrite(LED_LEFT_SEPARATOR, r, g, b);
+  L.ledDrawLetter(2, m / 10 + '0', r, g, b);
+  L.ledDrawLetter(3, m % 10 + '0', r, g, b);
+  L.ledWrite(LED_RIGHT_SEPARATOR, 0, 0, 0);
+  L.ledDrawLetter(4, '8', 0, 0, 0);
+  L.ledDrawLetter(5, '8', 0, 0, 0);
 }
 
 void setup(void) {
@@ -25,14 +36,7 @@ void loop(void) {
   r = 0xff; g = 0; b = 0;
   while(1) {
     L.adkEventProcess();
-    uint16_t year;
-    uint8_t month, day, h, m, s;
-    L.rtcGet(&year, &month, &day, &h, &m, &s);
-    L.ledDrawLetter(0, h / 10 + '0', r, g, b);
-    L.ledDrawLetter(1, h % 10 + '0', r, g, b);
-    L.ledDrawLetter(2, m / 10 + '0', r, g, b);
-    L.ledDrawLetter(3, m % 10 + '0', r, g, b);
-    L.ledDrawLetter(4, s / 10 + '0', r, g, b);
-    L.ledDrawLetter(5, s % 10 + '0', r, g, b);
+    displayCurrentTime(r, g, b);
+    L.ledDrawIcon(ICON_LOCK, r, g, b);
   }
 }
